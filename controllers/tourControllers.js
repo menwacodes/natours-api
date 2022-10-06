b4d7e1b94f02353ac7c76315c6ef890fda665148
Mike O'B-W <68826548+menwacodes@users.noreply.github.com>
2022-10-06 13:58:43 -0400

    Completed 8-92

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,20 +1,37 @@
 const Tour = require('./../models/tourModel.js');
 
-const getAllTours = (req, res) => {
-    res.status(200).json({
-        status: "success",
-        // results: tourRoutes.length,
-        // data: {
-        //     tours: tourRoutes
-        // }
-    });
+const getAllTours = async (req, res) => {
+    try {
+        const tours = await Tour.find();
+        res.status(200).json({
+            status: "success",
+            results: tours.length,
+            data: {tours}
+        });
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({
+            status: "failure",
+            message: e
+        });
+    }
 };
 
-const getOneTour = (req, res) => {
-    res.status(200).json({
-        status: "success",
-        // data: {tour: tourRoutes.find(tour => tour.id === +req.params.id)}
-    });
+const getOneTour = async (req, res) => {
+    try {
+        const tour = await Tour.findById(req.params.id); // because this is a param named id on the route
+        // findById is a shorthand helper for .findOne({_id: req.params.id})
+        res.status(200).json({
+            status: "success",
+            data: {tour}
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({
+            status: "failure",
+            message: `No tour with id ${req.params.id}`
+        });
+    }
 };
 
 const createTour = async (req, res) => {
@@ -37,24 +54,43 @@ const createTour = async (req, res) => {
     }
 };
 
-const updateTour = (req, res) => {
-    res.status(200).json({
-        status: "success",
-        data: {
-            tour: "Here is your updated tour"
-        }
-    });
+const updateTour = async (req, res) => {
+    try {
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new: true, // the new, updated document is what is returned
+            runValidators: true // ensures any created validators are run on the passed-in data
+        });
+        res.status(200).json({
+            status: "success",
+            data: {tour}
+        });
+    } catch (e) {
+        console.error("An error occurred", e);
+        res.status(400).json({
+            status: "failure",
+            message: e
+        });
+    }
 };
 
-const deleteTour = (req, res) => {
-    const id = +req.params.id;
-    res.status(204).json({
-        status: "success",
-        data: {
-            tour: null,
-            message: `Tour ${id} was deleted`
-        }
-    });
+const deleteTour = async (req, res) => {
+    const id = req.params.id;
+    try {
+        await Tour.findByIdAndDelete(id);
+        res.status(204).json({
+            status: "success",
+            data: {
+                tour: null,
+                message: `Tour ${id} was deleted`
+            }
+        });
+    } catch (e) {
+        console.error(e);
+        res.status(400).json({
+            status: "failure",
+            message: `No Tour with id: ${id}`
+        });
+    }
 };
 
 module.exports = {getAllTours, getOneTour, createTour, updateTour, deleteTour};
