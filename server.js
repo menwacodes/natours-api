d618c7588f6783498b71d43cd826848b3ce29645
Mike O'B-W <68826548+menwacodes@users.noreply.github.com>
2022-10-11 12:04:04 -0400

    Finished Error Handling

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // everything starts here, none of this is express specific
 const mongoose = require('mongoose');
+/*
+    Mongo authentication error error.message: "Authentication failed.", code: 8000, codeName: 'AtlasError'
+ */
 
 const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});
@@ -29,4 +32,17 @@ const connect = async () => {
 connect();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(c.bgGreenBright(`Listening on http://localhost:${port}`)));
+const server = app.listen(port, () => console.log(c.bgGreenBright(`Listening on http://localhost:${port}`)));
+
+// Catch any unhandled promise rejections
+process.on('unhandledRejection', err => {
+    console.log(c.bgRed.yellowBright(`Unhandled Rejection --- ${err.name}: ${err.message}`));
+    server.close(() => process.exit(1)); // graceful shutdown
+});
+
+// Uncaught Exceptions: errors in synchronous code
+process.on('uncaughtException', err => {
+    console.log(c.bgRed.yellowBright(`Uncaught Exception --- ${err.name}: ${err.message}`));
+    console.error(err)
+    process.exit(1) // because synchronous, nothing is waiting on it, should be at top of file
+});
